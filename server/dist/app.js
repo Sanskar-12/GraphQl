0fd819f2d69d0832abc7364f4f40c02fe5254ec9
Sanskar-12 <sanskarv2004@gmail.com>
2024-05-29 15:32:29 +0530

    server part completed

--- server/dist/app.js
+++ server/dist/app.js
@@ -3,6 +3,9 @@ import express from "express";
 import { connectDB } from "./database/database.js";
 import { connectGraphQl } from "./graphql/graphql.js";
 import { expressMiddleware } from "@apollo/server/express4";
+import { errorMiddleware } from "./middlewares/error.js";
+import cors from "cors";
+import morgan from "morgan";
 dotenv.config({ path: "./.env" });
 connectDB();
 export const envMode = process.env.NODE_ENV?.trim() || "DEVELOPMENT";
@@ -12,18 +15,18 @@ await server.start();
 const app = express();
 app.use(express.json());
 app.use("/graphql", expressMiddleware(server));
-// app.use(express.urlencoded({ extended: true }));
-// app.use(cors({ origin: " * ", credentials: true }));
-// app.use(morgan("dev"));
+app.use(express.urlencoded({ extended: true }));
+app.use(cors({ origin: " * ", credentials: true }));
+app.use(morgan("dev"));
 app.get("/", (req, res) => {
     res.send("Hello, World!");
 });
 // // your routes here
-// app.get("*", (req, res) => {
-//   res.status(404).json({
-//     success: false,
-//     message: "Page not found",
-//   });
-// });
-// app.use(errorMiddleware);
+app.get("*", (req, res) => {
+    res.status(404).json({
+        success: false,
+        message: "Page not found",
+    });
+});
+app.use(errorMiddleware);
 app.listen(port, () => console.log("Server is working on Port:" + port + " in " + envMode + " Mode."));
--- server/src/app.ts
+++ server/src/app.ts
@@ -3,6 +3,9 @@ import express, { NextFunction, Request, Response } from "express";
 import { connectDB } from "./database/database.js";
 import { connectGraphQl } from "./graphql/graphql.js";
 import { expressMiddleware } from "@apollo/server/express4";
+import { errorMiddleware } from "./middlewares/error.js";
+import cors from "cors";
+import morgan from "morgan";
 
 dotenv.config({ path: "./.env" });
 
@@ -20,9 +23,9 @@ const app = express();
 app.use(express.json());
 
 app.use("/graphql", expressMiddleware(server));
-// app.use(express.urlencoded({ extended: true }));
-// app.use(cors({ origin: " * ", credentials: true }));
-// app.use(morgan("dev"));
+app.use(express.urlencoded({ extended: true }));
+app.use(cors({ origin: " * ", credentials: true }));
+app.use(morgan("dev"));
 
 app.get("/", (req, res) => {
   res.send("Hello, World!");
@@ -30,14 +33,14 @@ app.get("/", (req, res) => {
 
 // // your routes here
 
-// app.get("*", (req, res) => {
-//   res.status(404).json({
-//     success: false,
-//     message: "Page not found",
-//   });
-// });
+app.get("*", (req, res) => {
+  res.status(404).json({
+    success: false,
+    message: "Page not found",
+  });
+});
 
-// app.use(errorMiddleware);
+app.use(errorMiddleware);
 
 app.listen(port, () =>
   console.log("Server is working on Port:" + port + " in " + envMode + " Mode.")
